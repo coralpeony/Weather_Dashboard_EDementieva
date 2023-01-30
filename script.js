ccffdebe2b4e869c7af0c726024ef3fb012bc840
Coral <coralpeony21@gmail.com>
2023-01-30 21:36:43 +0200

    added event handler for search button

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ const todayContainer = $("#today");
 const fiveDays = $("#5days");
 const apiKey = "8cd1ced871abc4840d6ef09b2a8f9080";
 
+$(".search-button").click(function (event) {
+  event.preventDefault();
+  
+  var queryParam = $("#search-input").val();
+
+  var weather =
+    "http://api.openweathermap.org/data/2.5/forecast?q=" +
+    queryParam +
+    "&units=metric&appid=" +
+    apiKey;
+
+  if (!queryParam) {
+    return;
+  }
+
+
+  
+});
+
+
+//$(".container-fluid").empty();
