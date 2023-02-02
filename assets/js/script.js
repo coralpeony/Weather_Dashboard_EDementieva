946632cd6e0773759c5847349e8aa9ac610bea96
Coral <coralpeony21@gmail.com>
2023-02-02 22:34:47 +0200

    added savedToLS function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,10 @@ const forecastContainer = $("#forecast");
 const todayContainer = $("#today");
 const fiveDays = $("#5days");
 const apiKey = "8cd1ced871abc4840d6ef09b2a8f9080";
+const searchHistory = []; 
+const history = $("#history");
+
+// pulling data from weatherAPI when the search button is clicked
 
 $(".search-button").click(function (event) {
   event.preventDefault();
@@ -26,20 +30,25 @@ $(".search-button").click(function (event) {
       console.log(data);
       displayForecastData(data);
       displayWeatherNow(data);
+      savedToLS(data, queryParam);
+      createButton(data.city.name);
     });
+});
+
+
+// display 5 day forecast 
 
   function displayForecastData(data) {
+    forecastContainer.empty();
+    fiveDays.empty();
     for (let index = 1; index < data.list.length; index += 8) {
       console.log(data.list[index]);
 
-      
       const cardContainer = $('<div class="myCard">');
       const dateDiv = $("<div>Date: " + data.list[index].dt_txt + "</div>");
-
       const icon = data.list[index].weather[0].icon;
       const imgURL = `https://openweathermap.org/img/wn/${icon}@2x.png`
       const img = $("<img>").attr("src", imgURL);
-
       const tempDiv = $(
         "<div>Temp: " + data.list[index].main.temp + " °C</div>"
       );
@@ -58,41 +67,51 @@ $(".search-button").click(function (event) {
       forecastContainer.append(cardContainer);
     
     }
-
+    
     const text = $("<h2>").text("5-Day Forecast:");
     fiveDays.prepend(text);
   }
 
-  function displayWeatherNow(data) {
-    // here is the data for the current day
-    const todayCardDiv = $('<div class="today">');
-    const cityName = $("<h2>" + data.city.name + "</h2>");
-    const todayDateDiv = $("<div>Date: " + data.list[0].dt_txt + "</div>");
-    
-    const todayIcon = data.list[0].weather[0].icon;
-    const todayImgURL = `https://openweathermap.org/img/wn/${todayIcon}@2x.png`
-    const todayImg = $("<img>").attr("src", todayImgURL);
+// display data for the current day
+function displayWeatherNow(data) {
+  todayContainer.empty();
+  
+  const todayCardDiv = $('<div class="today">');
+  const cityName = $("<h2>" + data.city.name + "</h2>");
+  const todayDateDiv = $("<div>Date: " + data.list[0].dt_txt + "</div>");
+  
+  const todayIcon = data.list[0].weather[0].icon;
+  const todayImgURL = `https://openweathermap.org/img/wn/${todayIcon}@2x.png`
+  const todayImg = $("<img>").attr("src", todayImgURL);
 
-    const todayTempDiv = $(
-      "<div>Temp: " + data.list[0].main.temp + " °C</div>"
-    );
-    const todayWindDiv = $(
-      "<div>Wind: " + data.list[0].wind.speed + " metre/sec</div>"
-    );
-    const todayHumidDiv = $(
-      "<div>Humidity: " + data.list[0].main.humidity + " %</div>"
-    );
+  const todayTempDiv = $(
+    "<div>Temp: " + data.list[0].main.temp + " °C</div>"
+  );
+  const todayWindDiv = $(
+    "<div>Wind: " + data.list[0].wind.speed + " metre/sec</div>"
+  );
+  const todayHumidDiv = $(
+    "<div>Humidity: " + data.list[0].main.humidity + " %</div>"
+  );
 
-    todayCardDiv.append(cityName);
-    todayCardDiv.append(todayDateDiv);
-    todayCardDiv.append(todayImg);
-    todayCardDiv.append(todayTempDiv);
-    todayCardDiv.append(todayWindDiv);
-    todayCardDiv.append(todayHumidDiv);
-    todayContainer.append(todayCardDiv);
+
+  todayCardDiv.append(cityName);
+  todayCardDiv.append(todayDateDiv);
+  todayCardDiv.append(todayImg);
+  todayCardDiv.append(todayTempDiv);
+  todayCardDiv.append(todayWindDiv);
+  todayCardDiv.append(todayHumidDiv);
+  todayContainer.append(todayCardDiv);
+  
+}
+
+// saved to local storage 
+
+function savedToLS(data, cityName) {
+  const cities = JSON.parse(localStorage.getItem("weather")) || []
+  console.log(data.city.name, cityName);
+  cities.push(data.city.name);
+  localStorage.setItem("weather", JSON.stringify(cities))
+  
   }
   
-});
-
-
-//$(".container-fluid").empty();
